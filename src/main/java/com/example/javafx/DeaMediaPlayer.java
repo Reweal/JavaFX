dc3428f258a37c39456a5d2f97716d8d0a348bb0
Michael Kurmaev <6oo6aa@mail.ru>
2023-08-07 11:06:39 +0300

    updated

--- src/main/java/com/example/javafx/DeaMediaPlayer.java
+++ src/main/java/com/example/javafx/DeaMediaPlayer.java
@@ -10,6 +10,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.Slider;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
@@ -18,6 +20,7 @@ import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.IOException;
+
 import javafx.util.Duration;
 
 public class DeaMediaPlayer extends Application {
@@ -27,10 +30,10 @@ public class DeaMediaPlayer extends Application {
 
     @FXML
     private Button playButton = new Button();
-
     @FXML
-    private Button pauseButton = new Button();
-
+    private Button onPauseButton = new Button();
+    @FXML
+    private Button offPauseButton = new Button();
     @FXML
     private Button stopButton = new Button();
     @FXML
@@ -65,15 +68,13 @@ public class DeaMediaPlayer extends Application {
         }
 
         FileChooser fileChooser = new FileChooser();
-        fileChooser.getExtensionFilters()
-                .add(new FileChooser.ExtensionFilter("Audio Files", "*.wav", "*.mp3", "*.aac"));
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Audio Files", "*.wav", "*.mp3", "*.aac"));
         selectedFile = fileChooser.showOpenDialog(null);
 
         if (selectedFile != null) {
             Media sound = new Media(selectedFile.toURI().toString());
             mediaPlayer = new MediaPlayer(sound);
             mediaPlayer.play();
-            pauseButton.setText("Pause");
             volumeSlider.setValue(1.0);
             controlBox.setVisible(true);
             musicProgressBar.setVisible(true);
@@ -81,8 +82,7 @@ public class DeaMediaPlayer extends Application {
 
         mediaPlayer.currentTimeProperty().addListener(new ChangeListener<Duration>() {
             @Override
-            public void changed(ObservableValue<? extends Duration> observableValue,
-                Duration oldValue, Duration newValue) {
+            public void changed(ObservableValue<? extends Duration> observableValue, Duration oldValue, Duration newValue) {
                 musicProgressBar.setProgress(newValue.toSeconds() / mediaPlayer.getTotalDuration().toSeconds());
             }
         });
@@ -94,22 +94,29 @@ public class DeaMediaPlayer extends Application {
                 }
             }
         });
+
         playButton.setVisible(false);
-        pauseButton.setVisible(true);
+        onPauseButton.setVisible(true);
+        offPauseButton.setVisible(false);
         stopButton.setVisible(true);
         volumeSlider.setVisible(true);
     }
 
     @FXML
     protected void onPauseButtonClick() {
-        if (mediaPlayer != null) {
-            if (mediaPlayer.getStatus() == MediaPlayer.Status.PLAYING) {
-                mediaPlayer.pause();
-                pauseButton.setText("Resume");
-            } else if (mediaPlayer.getStatus() == MediaPlayer.Status.PAUSED) {
-                mediaPlayer.play();
-                pauseButton.setText("Pause");
-            }
+        if (mediaPlayer != null && mediaPlayer.getStatus() == MediaPlayer.Status.PLAYING) {
+            mediaPlayer.pause();
+            onPauseButton.setVisible(false);
+            offPauseButton.setVisible(true);
+        }
+    }
+
+    @FXML
+    protected void offPauseButtonClick() {
+        if (mediaPlayer != null && mediaPlayer.getStatus() == MediaPlayer.Status.PAUSED) {
+            mediaPlayer.play();
+            onPauseButton.setVisible(true);
+            offPauseButton.setVisible(false);
         }
     }
 
@@ -119,9 +126,11 @@ public class DeaMediaPlayer extends Application {
             mediaPlayer.stop();
             selectedFile = null;
             playButton.setVisible(true);
-            pauseButton.setVisible(false);
+            onPauseButton.setVisible(false);
             stopButton.setVisible(false);
             volumeSlider.setVisible(false);
+            musicProgressBar.setVisible(false);
+            controlBox.setVisible(false);
         }
     }
 }
